8b633d61728dac9a07d79df6162654a2808d614d
Vadim Klimchuk <vadimklimchuk77@smartexlab.com>
2020-02-18 17:31:41 +0300

    fix routes for admin and referee

--- src/app/layouts/admin/admin.module.ts
+++ src/app/layouts/admin/admin.module.ts
@@ -6,6 +6,8 @@ import { MainComponent } from 'src/app/pages/main/main.component';
 import { RefereeListComponent } from 'src/app/pages/referee-list/referee-list.component';
 import { RunnerListComponent } from 'src/app/pages/runner-list/runner-list.component';
 import { ResultsComponent } from 'src/app/pages/results/results.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminRouteModule } from './admin-route.module';
 
 
 
@@ -16,9 +18,11 @@ import { ResultsComponent } from 'src/app/pages/results/results.component';
     RefereeListComponent,
     RunnerListComponent,
     ResultsComponent,
+    HeaderComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRouteModule
   ]
 })
 export class AdminModule { }
