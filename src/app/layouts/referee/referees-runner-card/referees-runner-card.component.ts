7ca6faf9e74fe28b25103ff38cb4593118c07e5f
AntonS <freesaber@yandex.ru>
2020-02-19 13:35:58 +0300

    make runner list dynamic

--- src/app/layouts/referee/referees-runner-card/referees-runner-card.component.ts
+++ src/app/layouts/referee/referees-runner-card/referees-runner-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-referees-runner-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./referees-runner-card.component.scss']
 })
 export class RefereesRunnerCardComponent implements OnInit {
+  @Input() id;
 
   constructor() { }
 
