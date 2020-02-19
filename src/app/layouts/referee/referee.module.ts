b2b1d1e91fca8ac95d2db0c0ba210ffad6ea6dd8
AntonS <freesaber@yandex.ru>
2020-02-19 12:10:47 +0300

    make runner cards

--- src/app/layouts/referee/referee.module.ts
+++ src/app/layouts/referee/referee.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { RefereeComponent } from './referee.component';
 import { RefereeRouteModule } from './referee-route.module';
 
-
+import { RefereesRunnerCardComponent } from './referees-runner-card/referees-runner-card.component';
 
 @NgModule({
   declarations: [
     RefereeComponent,
+    RefereesRunnerCardComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/layouts/referee/referees-runner-card/referees-runner-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referees-runner-card',
+  templateUrl: './referees-runner-card.component.html',
+  styleUrls: ['./referees-runner-card.component.scss']
+})
+export class RefereesRunnerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
