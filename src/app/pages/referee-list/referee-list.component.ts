73c817ab555df3afe356d258c34529da94919cba
AntonS <freesaber@yandex.ru>
2020-02-19 17:26:56 +0300

    results page

--- src/app/pages/referee-list/referee-list.component.ts
+++ src/app/pages/referee-list/referee-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+enum WizardTableHeader {
+  NAME = 'Имя',
+  RUNNERS = 'Спортсмены',
+  LOGIN = 'Логин',
+  PASSWORD = 'Пароль',
+}
+
 @Component({
   selector: 'app-referee-list',
   templateUrl: './referee-list.component.html',
   styleUrls: ['./referee-list.component.scss']
 })
 export class RefereeListComponent implements OnInit {
+  public tableTitles = Object.values(WizardTableHeader);
 
   public refereesMock = [
     {
